85c344c2083a84b4623805df0ccddb3ec9778066
haungyongkang <haungyongkang@xiaomai5.com>
2019-02-19 18:41:41 +0800

    init

---
+++ app/src/main/java/model/CollectionModel.java
@@ -0,0 +1,43 @@
+package model;
+
+import lombok.Data;
+
+@Data
+public class CollectionModel {
+    private int id;
+    private String phoneNumber;
+    private String poetryId;
+    private String poetryTitle;
+    private String collectTime;
+//
+//    public String getPoetryTitle() {
+//        return poetryTitle;
+//    }
+//    public void setPoetryTitle(String poetryTitle) {
+//        this.poetryTitle = poetryTitle;
+//    }
+//    public int getId() {
+//        return id;
+//    }
+//    public void setId(int id) {
+//        this.id = id;
+//    }
+//    public String getPhoneNumber() {
+//        return phoneNumber;
+//    }
+//    public void setPhoneNumber(String phoneNumber) {
+//        this.phoneNumber = phoneNumber;
+//    }
+//    public String getPoetryId() {
+//        return poetryId;
+//    }
+//    public void setPoetryId(String poetryId) {
+//        this.poetryId = poetryId;
+//    }
+//    public String getCollectTime() {
+//        return collectTime;
+//    }
+//    public void setCollectTime(String collectTime) {
+//        this.collectTime = collectTime;
+//    }
+}
